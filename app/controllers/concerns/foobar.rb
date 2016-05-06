8968850e1de4bbcceb6da9b8b05d38bec5ba7e4c
Mitalee Save <save.mitalee@gmail.com>
2016-05-06 23:25:14 +0000

    Question2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,15 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  
+  # Question2 
+  def initialize(b)
+     hash = Hash.new(b)
+     end
+     def bar(arg, args = {})
+         puts arg
+         args.each{|v|
+             v            
+         }
+         
+     end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   def home
     # foo = Foobar.new "baz"
     # @baz = foo.bar :cat, sat: :dat, dat: :sat
+  
+    # Question2 
+    foo = Foobar.new  "baz"
+     @baz = foo.bar :cat, sat: :dat, dat: :sat
+  
   end
 
   def stringify
